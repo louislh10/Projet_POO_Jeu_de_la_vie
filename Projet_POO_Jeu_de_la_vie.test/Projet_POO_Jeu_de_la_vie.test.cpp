c15e343856e9ec8d3db4f6d6550a7a064713e3c2
Louis Le Hel <louis.lehel@viacesi.fr>
2025-12-03 19:45:39 +0100

    ajout test verif enregistrement fichier

--- Projet_POO_Jeu_de_la_vie.test/Projet_POO_Jeu_de_la_vie.test.cpp
+++ Projet_POO_Jeu_de_la_vie.test/Projet_POO_Jeu_de_la_vie.test.cpp
@@ -2,29 +2,33 @@
 #include "CppUnitTest.h"
 
 #include <vector>
+#include <fstream>
 
 #include "../Jeu.h"
-#include "../Jeu.cpp"
 #include "../RegleC.h"
-#include "../RegleC.cpp"
 #include "../Cellule.h" 
-#include "../Cellule.cpp" 
 #include "../Grille.h"
-#include "../Grille.cpp"
 #include "../CelluleMorte.h" 
-#include "../CelluleMorte.cpp"
 #include "../CelluleVivante.h"
+#include "../Regle.h"
+#include <memory>
+#include "../Jeu.cpp"
+#include "../RegleC.cpp"
+#include "../Cellule.cpp" 
+#include "../Grille.cpp"
+#include "../CelluleMorte.cpp" 
 #include "../CelluleVivante.cpp"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace ProjetPOOJeudelaviedesLouistest
+namespace ProjetPOOJeudelavietest
 {
+    // Test des structures stables
     TEST_CLASS(TestGrilleSimple)
     {
     public:
-
-        TEST_METHOD(Blockstill_life)// cette grile doit quoi qu'il arrive ne pas bouger 
+        TEST_METHOD(Blockstill_life) // Cette grille doit rester stable
         {
             std::vector<std::vector<int>> blockinitial = {
                 {0,0,0,0},
@@ -33,19 +37,13 @@ namespace ProjetPOOJeudelaviedesLouistest
                 {0,0,0,0}
             };
 
-            std::vector<std::vector<int>> blockattendu = {
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,1,0},
-                {0,0,0,0}
-            };
-
+            std::vector<std::vector<int>> blockattendu = blockinitial;
             const int iter = 5;
 
             int h = static_cast<int>(blockinitial.size());
             int w = static_cast<int>(blockinitial[0].size());
 
-            Grille g(w, h);           
+            Grille g(w, h);
             g.initialiserDepuisMatrice(blockinitial);
 
             RegleC rule;
@@ -55,18 +53,60 @@ namespace ProjetPOOJeudelaviedesLouistest
 
             std::vector<std::vector<int>> matOut = g.convertirEnMatrice();
 
-            Assert::AreEqual(blockattendu.size(), matOut.size()); 
-
-			for (size_t i = 0; i < blockattendu.size(); ++i) //permet de comparer chaque ligne
+            Assert::AreEqual(blockattendu.size(), matOut.size());
+            for (size_t i = 0; i < blockattendu.size(); ++i)
             {
                 Assert::AreEqual(blockattendu[i].size(), matOut[i].size());
-
-				for (size_t j = 0; j < blockattendu[i].size(); ++j) //permet de comparer chaque élément de la ligne
+                for (size_t j = 0; j < blockattendu[i].size(); ++j)
                 {
                     Assert::AreEqual(blockattendu[i][j], matOut[i][j]);
                 }
             }
-
         }
     };
+
+    // Test de la sauvegarde après N itérations
+    TEST_CLASS(vérification_enregistrement_fichier)
+    {
+    public:
+        static const int iter = 5;
+
+        TEST_METHOD(Test_SaveAfterNIterations)
+        {
+            const std::string inputFile = "testfichier.txt";
+            const std::string outDirBase = "testfichier_out";
+            const std::string outPath = outDirBase + "/iter_" + std::to_string(iter) + ".txt";
+
+            // Création du jeu et chargement du fichier
+            Jeu jeu(1, 1, std::make_unique<RegleC>());
+            try {
+                jeu.chargerDepuisFichier(inputFile);
+            }
+            catch (const std::exception& e) {
+                Assert::Fail((L"Échec lecture fichier: " + std::wstring(e.what(), e.what() + strlen(e.what()))).c_str());
+            }
+
+            // Appliquer ITERATIONS mises à jour
+            for (int i = 0; i < iter; ++i) {
+                jeu.mettreAJour();
+            }
+
+            // Sauvegarde dans un fichier
+            try {
+                jeu.sauvegarderDansFichier(outPath);
+            }
+            catch (const std::exception& e) {
+                Assert::Fail((L"Échec écriture fichier: " + std::wstring(e.what(), e.what() + strlen(e.what()))).c_str());
+            }
+
+            // Vérifier que le fichier de sortie existe et n'est pas vide
+            std::ifstream f(outPath);
+            Assert::IsTrue(f.is_open(), L"Le fichier de sortie n'a pas été créé.");
+            f.seekg(0, std::ios::end);
+            Assert::IsTrue(f.tellg() > 0, L"Le fichier de sortie est vide.");
+        }
+    };
+    
+
+    
 }
